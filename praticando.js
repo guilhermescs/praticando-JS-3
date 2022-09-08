b3c2f10481d1f726423302fe918180093ab81e08
Guilherme Cardoso <cardosoghr@gmail.com>
2022-09-08 19:34:17 -0300

    first commit

---
+++ praticando.js
@@ -0,0 +1,39 @@
+/*
+Strings
+
+Usa o console.log() para apresentar conteudos na tela utilizando
+nos parênteses "" ou '' ou ``
+
+
+number
+
+Conforme o ECMAScript Standard (o padrão do orgão que padroniza o JavaScript) temos 9 tipos de dados organizados em 4 categorias:
+Primitive/Primitive Value
+Structural 
+Structural Primitive.
+
+Os primitivos são: 
+    String,
+    Number,
+    Boolean,
+    Undefined,
+    Symbol,
+    BigInt. 
+    
+Os estruturais são:
+    Object
+    Function. 
+    
+Além destes, temos o primitivo estrutural que consiste apenas no dado null
+
+*/
+
+//variavel
+
+//Var e let são muito parecidas, podendo variar durante o código, por exemplo:
+var clima ="quente"
+    console.log(clima)
+    clima = "frio"
+    console.log(clima)
+// Já a variavel const é constante, mantendo seu valor imutavel durante todo o código.
+
